92b63264fb75f5da702d58897444e881a933507e
AntoniaMair <antonia.mair@outlook.de>
2021-03-26 09:40:12 +0100

    4 files modified, 4 added

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-book-list',
+  templateUrl: './book-list.component.html',
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/image.ts
@@ -0,0 +1,6 @@
+export class Image {
+  //gleich benennen wie in Laravel für autmatisches mapping
+  constructor(public id:number, public url:string, public title:string){}
+
+
+}
