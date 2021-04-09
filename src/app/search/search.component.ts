79bf0b198bc812d928224c706699df802a65a071
AntoniaMair <antonia.mair@outlook.de>
2021-04-09 11:59:35 +0200

    1 file modified

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, tap, switchMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bs-search',
@@ -9,13 +11,19 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 export class SearchComponent implements OnInit {
 
   keyup = new EventEmitter<string>();
+  isLoading=false;
+  foundBooks: Book[] = [];
+
+  constructor(private bs:BookStoreService){}
 
   ngOnInit() {
-    this.keyup.pipe(debounceTime(500))
-    .pipe(distinctUntilChanged())
-    .subscribe(value=>{
-      console.log(value);
-    });
-  }
+  this.keyup.pipe(filter(term => term!=""))
+   .pipe(debounceTime(500))
+   .pipe(distinctUntilChanged())
+   .pipe(tap(() => this.isLoading = true))
+   .pipe(switchMap(searchTerm =>  this.bs.getAllSearch(searchTerm)))
+   .pipe(tap(() => this.isLoading = false))
+   .subscribe(books => this.foundBooks = books);
+ }
 
 }
