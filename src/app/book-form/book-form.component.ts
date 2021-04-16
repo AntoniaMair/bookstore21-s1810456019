a71b1b058f2c96d70eadb1a65b3699e5abd9f155
AntoniaMair <antonia.mair@outlook.de>
2021-04-16 10:06:10 +0200

    1 file modified

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BookFactory } from '../shared/book-factory';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -11,10 +12,29 @@ import { BookStoreService } from '../shared/book-store.service';
 export class BookFormComponent implements OnInit {
 
   bookForm: FormGroup;
+  book = BookFactory.empty();
+  isUpdatingBook = false;
 
   constructor(private fb:FormBuilder, private bs:BookStoreService, private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    const isbn = this.route.snapshot.params['isbn'];
+    if(isbn){
+      this.isUpdatingBook = true;
+      this.bs.getSingle(isbn).subscribe(book =>{
+        this.book = book;
+        this.initBook();
+      });
+    }
+    this.initBook();
+  }
+
+  initBook(){
+    //Wir bauen das Formular Modell
+    //Links der Wert des Form controls, rechts was wir zuweisen wollen
+    this.bookForm = this.fb.group({
+      title: [this.book.title, Validators.required]
+    });
   }
 
 }
