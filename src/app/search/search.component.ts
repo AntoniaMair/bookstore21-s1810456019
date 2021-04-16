4e8f8eb2cee86ac4796aa938a1f3f6b9c325626d
AntoniaMair <antonia.mair@outlook.de>
2021-04-16 09:25:15 +0200

    4 files modified

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,23 +1,23 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, tap, switchMap } from 'rxjs/operators';
-import { Book } from '../shared/book';
-import { BookStoreService } from '../shared/book-store.service';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { debounceTime, distinctUntilChanged, filter,tap,switchMap } from "rxjs/operators";
+import { Book } from "../shared/book";
+import { BookStoreService } from "../shared/book-store.service";
 
 @Component({
-  selector: 'bs-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "bs-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
 })
 export class SearchComponent implements OnInit {
-
   keyup = new EventEmitter<string>();
   isLoading=false;
   foundBooks: Book[] = [];
   @Output() bookSelected = new EventEmitter<Book>();
 
-  constructor(private bs:BookStoreService){}
+  constructor(private bs:BookStoreService){
+  }
 
-  ngOnInit() {
+ ngOnInit() {
   this.keyup.pipe(filter(term => term!=""))
    .pipe(debounceTime(500))
    .pipe(distinctUntilChanged())
