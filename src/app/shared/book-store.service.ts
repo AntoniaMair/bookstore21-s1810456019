42da90f88b3d4d7e7aeac40fbfa25998fdd2e50e
AntoniaMair <antonia.mair@outlook.de>
2021-04-23 09:14:13 +0200

    2 files modified, 1 added

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -6,45 +6,58 @@ import { Author, Book, Image } from './book';
 @Injectable()
 export class BookStoreService {
   private api = "https://bookstore21.s1810456019.student.kwmhgb.at/api";
-  constructor(private http:HttpClient) { 
-  }
-  getAll():Observable<Array<Book>>{
-    return this.http.get<Array<Book>>(`${this.api}/books`).
-      pipe(retry(3)).pipe(catchError(this.errorHandler));
-  }
-  getSingle(isbn:string):Observable<Book>{
-    return this.http.get<Book>(`${this.api}/book/${isbn}`).
-      pipe(retry(3)).pipe(catchError(this.errorHandler));
-  }
+ constructor(private http: HttpClient) {}
 
-  remove(isbn:String):Observable<any>{
-     return this.http.delete<Book>(`${this.api}/book/${isbn}`).
-      pipe(retry(3)).pipe(catchError(this.errorHandler));
-  }
-
-  getAllSearch(searchTerm:string):Observable<Array<Book>>{
+  getAll(): Observable<Array<Book>> {
     return this.http
-    .get<Array<Book>>(`${this.api}/books/search/${searchTerm}`)
-    .pipe(retry(3))
-    .pipe(catchError(this.errorHandler));
+      .get<Array<Book>>(`${this.api}/books`)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
   }
 
-  create(book:Book):Observable<any>{
-     return this.http
-     .post<Book>(`${this.api}/book`, book)
-     .pipe(retry(3))
-     .pipe(catchError(this.errorHandler));
+  getSingle(isbn: string): Observable<Book> {
+    return this.http
+      .get<Book>(`${this.api}/book/${isbn}`)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
   }
 
-   update(book:Book):Observable<any>{
-     return this.http
-     .put<Book>(`${this.api}/book/${book.isbn}`, book)
-     .pipe(retry(3))
-     .pipe(catchError(this.errorHandler));
+  remove(isbn: String): Observable<any> {
+    return this.http
+      .delete(`${this.api}/book/${isbn}`)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
   }
 
-  private errorHandler(error:Error | any){
+  getAllSearch(searchTerm: string): Observable<Array<Book>> {
+    return this.http
+      .get<Array<Book>>(`${this.api}/books/search/${searchTerm}`)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
+  }
+
+  create(book: Book): Observable<any> {
+    return this.http
+      .post(`${this.api}/book`, book)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
+  }
+
+  update(book: Book): Observable<any> {
+    return this.http
+      .put(`${this.api}/book/${book.isbn}`, book)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: Error | any) {
     return throwError(error);
   }
 
+  check(isbn: String): Observable<Boolean> {
+    return this.http
+      .get<Boolean>(`${this.api}/books/checkisbn/${isbn}`)
+      .pipe(retry(3))
+      .pipe(catchError(this.errorHandler));
+  }
 }
